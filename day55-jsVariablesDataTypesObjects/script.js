9935476c9c4d4c7fd7ff97611db8197222414534
Rupin <rupinmaharjan07@gmail.com>
2024-03-07 20:38:43 +0545

    condition and loops

---
+++ day55-jsVariablesDataTypesObjects/script.js
@@ -0,0 +1,23 @@
+//console.log is used to print in console
+console.log("Today is day53");
+
+var a=5;
+let b=8;
+let c="Rupin";
+let d=6.78;
+
+console.log(a+b);
+{
+    let a=56;
+    console.log(a+b);
+
+}
+console.log(typeof a,typeof b,typeof c,typeof d);
+
+let obj={
+    "name":"Rupin",
+    "roll":45,
+    "address":"Thecho"
+}
+obj.phone=9861774585
+console.log(obj);
---
+++ day56-conditionals/index.js
@@ -0,0 +1,14 @@
+console.log("Conditional tut");
+let x = 4;
+// let x=prompt("Enter number:");
+
+if (x % 2 === 0) {
+    console.log(`${x} is even`);
+} else {
+    console.log(`${x} is odd`);
+}
+
+a=56;
+b=60;
+c=a>b?(a-b):(b-a);
+console.log(c);
